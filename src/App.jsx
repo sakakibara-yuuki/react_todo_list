2c8f09b8050ea1779269f923d10c35f01cddb397
sakakibara yuuki <sakakilabo0000@gmail.com>
2024-07-15 01:55:40 +0900

    feat: update style

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,25 @@ function HeaderLogo() {
   );
 }
 
+          // <path d="M 0,0 v 100,0 q 150,50 300,0 t 300,0 q 150,50 300,0 v 0,-100 Z" />
 function Progress() {
+  let start = 50;
+  let wave = `M 0,300 v -${start},0 q 150,-15 300,0 t 300,0 q 150,-15 300,0 v ${start} Z`;
   return (
     <div className="progress">
+      <div className="progress-metrics">
+        <div>100</div>
+        <div className="progress-metrics_inside">
+          <div>10</div>
+          <div>90</div>
+        </div>
+      </div>
       <div className="progress-container">
         <svg className="progress-wave"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 300">
-          <path d="M 0,0 v 100,0 q 150,50 300,0 t 300,0 q 150,50 300,0 v 0,-100 Z" />
+          <path d={wave} />
+        </svg>
+        <svg>
+          <path d={wave} />
         </svg>
       </div>
     </div>
